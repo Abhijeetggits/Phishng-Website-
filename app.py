65b12d818c1a7dd242fb742e9d44beb55106e7dc
Abhijeet Patel <abhijeetpatel176@gmail.com>
2025-11-01 23:06:46 +0530

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pickle
 import re
+import os
 
 app = Flask(__name__)
 
@@ -35,4 +36,5 @@ def index():
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
